c90689a1a9077b140d6f6bec7fd21138e126504e
Desislava Dakova <dessisdakova@gmail.com>
2025-01-17 15:27:29 +0200

    Tests for database

---
+++ tests/db/test_database.py
@@ -0,0 +1,18 @@
+from tests.db.fixtures import *
+from test_support.common.yaml_loaders import load_db_test_data
+
+
+@pytest.mark.parametrize("test_data", load_db_test_data("db_test_data")["queries"])
+def test_provided_queries(db_connection, test_data, db_logger):
+    # arrange
+    query = test_data["query"]
+    db_logger.debug(f"Query for execution: {query}.")
+    expected = [tuple(row) for row in test_data["expected_results"]]
+    db_logger.debug(f"Executed query for '{test_data['description']}'.")
+
+    # act
+    actual = db_connection.execute_query(query)
+
+    # assert
+    assert expected == actual, f"Result mismatch:\n Expected: {expected} \n Actual: {actual}\n"
+
