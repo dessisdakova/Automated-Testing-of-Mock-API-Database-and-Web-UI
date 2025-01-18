0762f21cdb4c0294176b99a7720bf454a4199d5d
Desislava Dakova <dessisdakova@gmail.com>
2025-01-18 11:56:19 +0200

    POM for web ui tests

---
+++ test_support/helpers/web_ui/checkout_two_page.py
@@ -0,0 +1,49 @@
+from test_support.helpers.web_ui.base_page import BasePage
+from test_support.helpers.web_ui.locators import *
+
+
+class CheckoutTwoPage(BasePage):
+    """
+    Page object for the checkout step two page.
+    Inherits from BasePage.
+    """
+    @property
+    def base_url(self) -> str:
+        """
+        The URL of the checkout step two page.
+
+        :return: A string representing the page URL.
+        """
+        return super().base_url + "checkout-step-two.html"
+
+    @property
+    def explicit_wait_locator(self) -> tuple:
+        """
+        Locator for the element used to verify the checkout step two page has loaded.
+
+        :return: Locator as tuple.
+        """
+        return SUMMARY_INFO_DIV
+
+    def click_finish_button(self) -> None:
+        """
+        Click the finish button to complete the order process.
+        """
+        self.driver.find_element(*FINISH_BUTTON).click()
+
+    def get_items_count_in_order(self) -> int:
+        """
+        Retrieve all items in the order.
+
+        :return: The number of items added to the cart.
+        """
+        return len(self.driver.find_elements(*ITEMS_IN_CART))
+
+    def get_item_names_in_cart(self) -> list:
+        """
+        Retrieve the names of all items in the cart.
+
+        :return: A list of item names added to the cart.
+        """
+        elements = self.driver.find_elements(*ITEMS_NAME_IN_CART)
+        return [element.text for element in elements]
---
+++ test_support/helpers/web_ui/inventory_page.py
@@ -0,0 +1,56 @@
+from test_support.helpers.web_ui.base_page import BasePage
+from test_support.helpers.web_ui.locators import *
+
+
+class InventoryPage(BasePage):
+    """
+    Page object for the inventory page.
+    Inherits from BasePage.
+    """
+    @property
+    def base_url(self) -> str:
+        """
+        The URL of the inventory page.
+
+        :return: A string representing the inventory page URL.
+        """
+        return super().base_url + "inventory.html"
+
+    @property
+    def explicit_wait_locator(self) -> tuple:
+        """
+        Locator for the element used to verify the inventory page has loaded.
+
+        :return: Locator tuple for the inventory container.
+        """
+        return INVENTORY_CONTAINER
+
+    def add_item_to_cart(self, item: str) -> None:
+        """
+        Add item to cart based on the following values:
+         - "backpack" - adds Sauce Labs Backpack
+         - "bike-light" - adds Sauce Labs Bike Light
+
+        :param item: Must be one of the provided values.
+        :return: None
+        :raises ValueError: If the value of item does not match the provided ones.
+        """
+        if item == "backpack":
+            self.driver.find_element(*ADD_BACKPACK_BUTTON).click()
+        elif item == "bike-light":
+            self.driver.find_element(*ADD_BIKE_LIGHT_BUTTON).click()
+        else:
+            raise ValueError(f"Unknown item '{item}'!")
+
+    def get_items_count_in_cart(self) -> int:
+        """
+        Retrieve items count in cart from shopping cart badge.
+        """
+        items = self.driver.find_element(*SHOPPING_CART_BADGE).text
+        return int(items)
+
+    def go_to_cart(self) -> None:
+        """
+        Navigate to the shopping cart page by clicking the cart link.
+        """
+        self.driver.find_element(*SHOPPING_CART_LINK).click()
